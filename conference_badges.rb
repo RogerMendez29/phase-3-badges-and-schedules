c2cad9edd2103cdfd4d76027ef21a5ad43848b1c
Roger Mendez <roger61087@gmail.com>
2021-12-13 11:31:47 -0500

    commit tree 52d2f9f6d7c1ecf10a7adb9f3897c3cfba761093

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
-# Write your code here.
+require "pry"
+
+def badge_maker (name)
+    "Hello, my name is #{name}."
+    
+end
+
+def batch_badge_creator (array)
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+    
+end
+
+def assign_rooms (array)
+    
+
+     assignments= array.map.with_index(1) do |name,index|
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+        # binding.pry
+    end
+    assignments
+    
+end
+
+
+
+names_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def printer(array)
+badge = batch_badge_creator(array)
+badge.each{|badge| puts badge}
+
+assignments= assign_rooms(array)
+assignments.each{|assignment| puts assignment}
+
+
+    
+end
+
+
+ printer(names_array)
